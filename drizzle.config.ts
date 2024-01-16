c3e1657767151288313ae910d11652f84fcb67d6
Ole Magnus Fon Johnsen <me@omfj.no>
2024-01-16 16:23:52 +0100

    Update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,28 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
+const baseConfig = defineConfig({
 	out: './drizzle/migrations',
 	schema: './src/lib/db/schemas',
-	driver: 'libsql',
-	dbCredentials: {
-		url: process.env.DATABASE_URL!
-	},
 	strict: true,
 	verbose: true
 });
+
+const devConfig = defineConfig({
+	...baseConfig,
+	driver: 'libsql',
+	dbCredentials: {
+		url: process.env.DATABASE_URL!
+	}
+});
+
+const prodConfig = defineConfig({
+	...baseConfig,
+	driver: 'turso',
+	dbCredentials: {
+		url: process.env.DATABASE_URL!,
+		authToken: process.env.DATABASE_AUTH_TOKEN!
+	}
+});
+
+export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
