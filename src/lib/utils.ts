f6f5e0a9f50468c7adc985912790e33dc7e1e9b8
Ole Magnus Fon Johnsen <me@omfj.no>
2024-07-11 20:01:23 +0200

    Fix typo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 
-export const baseURL = dev ? 'http://localhost:5173' : 'https://uib.fyi';
+export const baseURL = dev ? 'http://localhost:5173' : 'https://timeplan.uib.fyi';
 
 export const unique = <T>(array: Array<T>) => [...new Set(array)];
 
